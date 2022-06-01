362572f025d2b3d8c79c776a36b17bb83f5a4ad5
AndreaVanElmpt <oibibio@gmail.com>
2022-06-01 22:03:31 +0200

    menu gemaakt

---
+++ src/pages/home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function HomePage(props) {
+    return (
+
+        <div className="page-container">
+            <h1>Prived Blog</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi assumenda consequuntur et eum ipsum, nam non numquam odit optio voluptate!</p>
+        </div>
+
+
+
+    );
+}
+
+export default HomePage;
